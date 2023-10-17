cbd4a5e465c7c40ecef562b4eae4c63c88dfea3f
Ilstr <ildarzmey@gmail.com>
2023-10-17 23:12:26 +0500

    V 2.0

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
 var romanNumbers = map[string]int{
 	"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5,
 	"VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10,
-	"XL": 40, "L": 50, "XC": 90, "C": 100,
 }
 
 // Конвертация из арабских в римские числа
